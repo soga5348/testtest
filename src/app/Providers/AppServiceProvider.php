b0ec12a188f58047d1d4a12f9c98c346a580ef77
Your Name <your_email@example.com>
2023-09-18 19:29:05 +0900

    a

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,13 +17,17 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+    public function boot(UrlGenerator $url)
+    {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+    }
+
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
-    {
-        //
-    }
+    
 }
