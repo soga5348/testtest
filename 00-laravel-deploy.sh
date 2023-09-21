cc7f820e772b0bfdb8d92edcecf7acb69d2c08c5
Your Name <your_email@example.com>
2023-09-21 14:24:28 +0900

    更新

--- 00-laravel-deploy.sh
+++ 00-laravel-deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/public
+composer install --no-dev --working-dir=/var/www
 
 echo "Caching config..."
 php artisan config:cache
 
-echo "Caching routes..."
+echo "Caching routes..."sd
 php artisan route:cache
 
 echo "Running migrations..."
